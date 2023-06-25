d4ac1c3cf1a382160640f68644d183483695a1af
early-dew <_mineral_@mail.com>
2023-06-25 16:52:35 +0200

    frontend url

--- frontend/src/services/peoples.js
+++ frontend/src/services/peoples.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons/'
-
+// const baseUrl = 'http://localhost:3001/api/persons/'
+const baseUrl = 'https://phonebook-backend-zdpi.onrender.com'
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
